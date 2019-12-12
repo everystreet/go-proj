773f3a7a6119b6d2269c6a0307d5e505037b0bab
Joe Mann <contact@joemann.co.uk>
2019-12-12 13:39:04 +0100

    Sample "cs2cs".

--- cmd/main.go
+++ cmd/main.go
@@ -11,6 +11,22 @@ import (
 )
 
 func main() {
+	var source, target string
+	to := false
+	for _, arg := range os.Args[1:] {
+		switch {
+		case arg == "+to":
+			to = true
+		case to:
+			target += " " + arg
+		default:
+			source += " " + arg
+		}
+	}
+
+	source = strings.TrimSpace(source)
+	target = strings.TrimSpace(target)
+
 	for {
 		r := bufio.NewReader(os.Stdin)
 		str, _ := r.ReadString('\n')
@@ -38,9 +54,7 @@ func main() {
 			Z: getFloat(2),
 		}
 
-		proj.CRSToCRS("EPSG:4326", "EPSG:32632",
-			proj.TransformForward(coord, &coord))
-
+		proj.CRSToCRS(source, target, proj.TransformForward(&coord))
 		fmt.Printf("%.2f\t%.2f\t%.2f\n", coord.X, coord.Y, coord.Z)
 	}
 }
